02e2a0b51b26b8e4794c87c64e1c7ac5092276da
Ankur Kumar <ankurkumar78@gmail.com>
2025-11-28 19:01:57 -0800

    added deployment instructions

--- app/financial_advisor_agent/deployment/deploy-cloud-run.py
+++
@@ -1,3 +0,0 @@
-export APP_NAME=financial-advisor-agent 
-export SERVICE_NAME=financial-advisor-agent 
-adk deploy cloud_run  --project=$GOOGLE_CLOUD_PROJECT --region=$GOOGLE_CLOUD_REGION --app_name=$APP_NAME  --service_name=$SERVICE_NAME --with_ui financial_advisor_agent
